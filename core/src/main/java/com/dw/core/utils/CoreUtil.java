1103a2facb0f523b9fe8a89f02f7d0e0c4903b57
UFO <627375202@qq.com>
2022-05-14 11:20:15 +0800

    create core module

---
+++ core/src/main/java/com/dw/core/utils/CoreUtil.java
@@ -0,0 +1,16 @@
+package com.dw.core.utils;
+
+/**
+ * 日期：2022/5/14
+ * <p>
+ * 作者：xudiwei
+ * <p>
+ * 描述：
+ */
+public class CoreUtil {
+
+
+    public static boolean isCore(){
+        return true;
+    }
+}
