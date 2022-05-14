23a4477a71aebb712ad04b5bfcd884b3e26ac8ac
UFO <627375202@qq.com>
2022-05-14 11:12:34 +0800

    add util

---
+++ ui-libs/src/main/java/com/dw/ui_libs/Util.java
@@ -0,0 +1,18 @@
+package com.dw.ui_libs;
+
+import android.util.Log;
+
+/**
+ * 日期：2022/5/14
+ * <p>
+ * 作者：xudiwei
+ * <p>
+ * 描述：
+ */
+public class Util {
+    private static final String TAG = "Util";
+
+    public static void showMessage(){
+        Log.d(TAG, "showMessage: ui-libs");
+    }
+}
